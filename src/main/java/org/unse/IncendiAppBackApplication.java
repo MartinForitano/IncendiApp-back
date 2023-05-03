f368e793dbc45d95c44388c7f5f1f18fa2b109c9
MartinForitano <martin005_11@hotmail.com>
2023-05-02 21:39:31 -0300

    Primer commit

---
+++ src/main/java/org/unse/IncendiAppBackApplication.java
@@ -0,0 +1,13 @@
+package org.unse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncendiAppBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncendiAppBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/unse/IncendiAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.unse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncendiAppBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
